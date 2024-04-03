a2cbebe434c575ac40228f4c38815fa6fef21f1b
AmirHSoukhteh <ascr.amir@gmail.com>
2024-04-03 12:15:37 +0330

    create and add first app with three views : index, about, contact

---
+++ mybrand/website/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index_view(request):
+    return HttpResponse('<h1><center> Home page</center> </h1>')
+def about_view(request):
+    return HttpResponse('<h1> About page </h1>')
+def contact_view(request):
+    return HttpResponse('<h1> Contact page </h1>')
